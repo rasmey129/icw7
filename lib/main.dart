3b089afba181d74fcbf8bd66de6b62c078b72574
Rasmey Oung <158348034+rasmey129@users.noreply.github.com>
2024-10-03 17:53:27 -0400

    added curve and AnimatedOpacity

--- lib/main.dart
+++ lib/main.dart
@@ -33,13 +33,16 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
       appBar: AppBar(
         title: Text('Fading Text Animation'),
       ),
-      body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 1),
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
+       body: Center(
+        child: GestureDetector(
+          onTap: toggleVisibility, 
+          child: AnimatedOpacity(
+            opacity: _isVisible ? 1.0 : 0.0, 
+            duration: const Duration(seconds: 1), 
+            child: const Text(
+              'Hello, Flutter!',
+              style: TextStyle(fontSize: 24),
+            ),
           ),
         ),
       ),
