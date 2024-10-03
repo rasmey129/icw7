5917630a1269d7bd8d2bce21aaa02343f609a6b0
Rasmey Oung <158348034+rasmey129@users.noreply.github.com>
2024-10-03 18:17:49 -0400

    added the image for the frame

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,12 @@ class FadingTextAnimation extends StatefulWidget {
 }
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
-  bool _isVisible = true;
+  bool _isVisible = true; 
+  bool _showFrame = false; 
 
   void toggleVisibility() {
     setState(() {
-      _isVisible = !_isVisible;
+      _isVisible = !_isVisible; 
     });
   }
 
@@ -31,19 +32,42 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Fading Text Animation'),
+        title: Text('Fading Text and Image Frame '),
       ),
-       body: Center(
-        child: GestureDetector(
-          onTap: toggleVisibility, 
-          child: AnimatedOpacity(
-            opacity: _isVisible ? 1.0 : 0.0, 
-            duration: const Duration(seconds: 1), 
-            child: const Text(
-              'Hello, Flutter!',
-              style: TextStyle(fontSize: 24),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: toggleVisibility, 
+              child: AnimatedOpacity(
+                opacity: _isVisible ? 1.0 : 0.0,
+                duration: const Duration(seconds: 1),
+                child: const Text(
+                  'Hello, Flutter!',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
             ),
-          ),
+            
+            Container(
+              decoration: _showFrame? BoxDecoration(
+                      border: Border.all(color: Colors.blue, width: 2), 
+                      borderRadius: BorderRadius.circular(16), 
+                    )
+                  : null, 
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16), 
+                child: Image.network(
+                  'https://cdn-7.motorsport.com/images/amp/YKEZbVX0/s1000/ferrari-499p-1.jpg', 
+                  width: 200, 
+                  height: 100,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
